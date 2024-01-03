cc91f5225d33d718f264f490d92413797050fd99
jihyunni <113456378+jihyunni@users.noreply.github.com>
2024-01-03 17:14:59 +0900

    Title: 특정 조건 세 정수 비교, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240103/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240103/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -4,28 +4,12 @@ a = A[0]
 b = A[1]
 c = A[2]
 
-if a>b and a>c:
-    if a == A[0]:
-        a = 1
-    else:
-        a = 0
-
-if a<b and b>c:
-    if b == A[0]:
-        a = 1
-    else:
-        a = 0
-
-
-if a<c and c>b:
-    if b == A[0]:
-        a = 1
-    else:
-        a = 0
+if a<=b and a<=c:
+    print(1, end = " ")
+else:
+    print(0, end = " ")
 
 if a==b==c:
-    aa = 1
+    print(1)
 else:
-    aa = 0
-
-print(a,aa)
+    print(0)
