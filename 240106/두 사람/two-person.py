8ae73f14cfb8102b61e03f965b5c8fdaa4484fc8
jihyunni <113456378+jihyunni@users.noreply.github.com>
2024-01-06 22:25:24 +0900

    Title: 두 사람, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240106/두 사람/two-person.py
+++ 240106/두 사람/two-person.py
@@ -1,15 +1,11 @@
-fir = list(map(str,input().split()))
-sec = list(map(str,input().split()))
+a,b = input().split()
 
-aa = int(fir[0])
-ase = fir[1]
+c,d = input().split()
 
-ba = int(sec[0])
-bse = sec[1]
 
-if aa >= 19 or ba >= 19:
-    if ase == 'M' or bse == 'M':
-        print(1)
-    else:
-        print(0)
-else: print(0)
+if int(a) >= 19 and b == 'M' :
+    print(1)
+elif int(c) >= 19 and d == 'M':
+    print(1)
+else :
+    print(0)
